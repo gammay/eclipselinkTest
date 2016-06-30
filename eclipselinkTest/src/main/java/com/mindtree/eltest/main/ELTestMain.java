6feac0ca112cf76e40e155f663c689d48454c510
amrita-j <amrita.jhunjhunwala@mindtree.com>
2016-06-30 17:47:46 +0530

    Added getCurrentChange code

--- eclipselinkTest/src/main/java/com/mindtree/eltest/main/ELTestMain.java
+++ eclipselinkTest/src/main/java/com/mindtree/eltest/main/ELTestMain.java
@@ -16,21 +16,31 @@ public class ELTestMain {
 
 
 	public static void main(String[] args) {
+		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager em = factory.createEntityManager();
 		
+		//transaction begins
+		em.getTransaction().begin();
 		
-		User user = new User(1, "user1-edited");
+		//getting existing user
+		User user = em.find(User.class, 1);
+		
+		//adding address
 		UserAddress ua = new UserAddress();
 		ua.setAddressLine1("line1");
 		ua.setAddressLine2("line2");
-		//ua.setTenant(new Tenant(1, ""));
-		user.setUserAddressList(Arrays.asList(new UserAddress[]{ua}));
-		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-	    EntityManager em = factory.createEntityManager();
-	    em.getTransaction().begin();
-	    em.merge(user);
-	    em.getTransaction().commit();
-	    em.close();
-	    
+		// ua.setTenant(new Tenant(1, ""));
+		user.setUserAddressList(Arrays.asList(new UserAddress[] { ua }));
+
+		//adding getCurrentChange manually
+		final JpaEntityManager jpaEntityManager = (JpaEntityManager) em.getDelegate();
+		jpaEntityManager.getUnitOfWork().getCurrentChanges();
+		
+		//final merge
+		em.merge(user);
+		System.out.println("..user before flush..." + user);
+		em.flush();
+		em.close();
 
 	}
 
